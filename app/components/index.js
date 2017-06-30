3baba2365efb5c611995f982198f71bf1d1bbef5
you <you@example.com>
2017-06-29 21:41:11 -0300

    first commit

---
+++ app/components/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MainComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hola Mundo!</h1>
+      </div>
+    );
+  }
+}
